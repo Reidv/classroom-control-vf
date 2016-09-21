0ad1c242492fa8e0a70b1e15f8987b27dd9c3113
Reidv <reidvacek@hotmail.com>
2016-09-21 16:14:53 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,10 +1,11 @@
 class skeleton {
 
 file { '/etc/skel':
-ensure => directory
+ensure => directory,
 }
 
 file { '/etc/skel/.bashrc':
+ensure => file,
 source => "/site/skeleton/files/bashrc",
 }
 
