d4456d47a0af153f96d673d942e161b7f4de2336
Reidv <reidvacek@hotmail.com>
2016-09-21 14:00:18 -0500

    user
    add user fundamental

---
+++ site/users/manifests/init.pp
@@ -0,0 +1,11 @@
+user { 'fundamentals':
+ 			  ensure           => 'present',
+      home             => '/home/fundamentals',
+      comment           => 'fundamentals',
+      groups            => 'web',
+      password         => '!!',
+      password_max_age => '99999',
+      password_min_age => '0',
+      shell            => '/bin/bash',
+      uid              => '501',
+    }
